dd9aa96359bf905cd7126b58ba4b1be51d019f3f
Yasuhiro Matsunaga <ymatsunaga@riken.jp>
2018-08-26 17:10:05 +0900

    add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,51 @@
 using MDToolbox
-@static if VERSION < v"0.7.0-DEV.2005"
-    using Base.Test
-else
-    using Test
+using Test
+
+@testset "TrjArray operations" begin
+    @testset "null object" begin
+        @test TrjArray().natom == 0
+        @test TrjArray().nframe == 0
+    end
+
+    @testset "slicing" begin
+        natom = 5
+        nframe = 5
+        ta = TrjArray(x=rand(nframe, natom), y=rand(nframe, natom), z=rand(nframe, natom))
+
+        @test ta.natom == natom
+        @test ta.nframe == nframe
+
+        @test ta[1, :].x ≈ ta.x[1:1, :]
+        @test ta[1:3, :].x ≈ ta.x[1:3, :]
+        @test ta[[1, 3, 5], :].x ≈ ta.x[[1, 3, 5], :]
+        @test ta[[true, true, true, false, false], :].x ≈ ta.x[[true, true, true, false, false], :]
+
+        @test ta[:, 1].x ≈ ta.x[:, 1:1]
+        @test ta[:, 1:3].x ≈ ta.x[:, 1:3]
+        @test ta[:, [1, 3, 5]].x ≈ ta.x[:, [1, 3, 5]]
+        @test ta[:, [true, true, true, false, false]].x ≈ ta.x[:, [true, true, true, false, false]]
+    end
+
+    @testset "atom selection" begin
+        natom = 5
+        nframe = 5
+        ta = TrjArray(x=rand(nframe, natom), y=rand(nframe, natom), z=rand(nframe, natom), atomid=1:natom, chainname=fill("A", natom))
+        @test ta["atomid 1:3"].x ≈ ta.x[:, 1:3]
+        @test ta["chainname A"].x ≈ ta.x[:, :]
+        @test ta["atomid 3:5 and chainname A"].x ≈ ta.x[:, 3:5]
+        @test ta["atomid 3:5 or chainname A"].x ≈ ta.x[:, :]
+        @test ta["(atomid 3:5 and chainname A) and atomid 3"].x ≈ ta.x[:, 3]
+        @test ta["(atomid 3:5 and chainname A) and (atomid 3 and chainname A)"].x ≈ ta.x[:, 3]
+    end
+
+    @testset "merging" begin
+        natom = 5
+        nframe = 5
+        topology = TrjArray(atomid=1:natom)
+        ta = TrjArray(x=rand(nframe, natom), y=rand(nframe, natom), z=rand(nframe, natom))
+
+        ta_new = [topology; ta]
+        ta_new["atomid 1:3"].x ≈ ta.x[:, 1:3]
+    end
 end
 
-# write your own tests here
-@test 1 == 2
