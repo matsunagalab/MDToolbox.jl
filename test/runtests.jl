e726a439ed820970267ac876ee0f832688d6341d
Yasuhiro Matsunaga <ymatsunaga@riken.jp>
2018-08-26 17:32:49 +0900

    fix a trjarray.jl bug related to boxsize

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,19 +1,25 @@
 using MDToolbox
 using Test
 
-@testset "TrjArray operations" begin
+@testset "trjarray.jl" begin
     @testset "null object" begin
         @test TrjArray().natom == 0
         @test TrjArray().nframe == 0
     end
 
-    @testset "slicing" begin
-        natom = 5
+    @testset "constructor" begin
         nframe = 5
+        natom = 5
         ta = TrjArray(x=rand(nframe, natom), y=rand(nframe, natom), z=rand(nframe, natom))
 
         @test ta.natom == natom
         @test ta.nframe == nframe
+    end
+
+    @testset "slicing" begin
+        nframe = 5
+        natom = 5
+        ta = TrjArray(x=rand(nframe, natom), y=rand(nframe, natom), z=rand(nframe, natom))
 
         @test ta[1, :].x ≈ ta.x[1:1, :]
         @test ta[1:3, :].x ≈ ta.x[1:3, :]
@@ -26,10 +32,22 @@ using Test
         @test ta[:, [true, true, true, false, false]].x ≈ ta.x[:, [true, true, true, false, false]]
     end
 
-    @testset "atom selection" begin
-        natom = 5
+    @testset "slicing with boxsize" begin
         nframe = 5
+        natom = 5
+        ta = TrjArray(x=rand(nframe, natom), y=rand(nframe, natom), z=rand(nframe, natom), boxsize=rand(nframe, 3))
+
+        @test ta[1, :].boxsize ≈ ta.boxsize[1:1, :]
+        @test ta[1:3, :].boxsize ≈ ta.boxsize[1:3, :]
+        @test ta[[1, 3, 5], :].boxsize ≈ ta.boxsize[[1, 3, 5], :]
+        @test ta[[true, true, true, false, false], :].boxsize ≈ ta.boxsize[[true, true, true, false, false], :]
+    end
+
+    @testset "atom selection" begin
+        nframe = 5
+        natom = 5
         ta = TrjArray(x=rand(nframe, natom), y=rand(nframe, natom), z=rand(nframe, natom), atomid=1:natom, chainname=fill("A", natom))
+
         @test ta["atomid 1:3"].x ≈ ta.x[:, 1:3]
         @test ta["chainname A"].x ≈ ta.x[:, :]
         @test ta["atomid 3:5 and chainname A"].x ≈ ta.x[:, 3:5]
@@ -39,13 +57,17 @@ using Test
     end
 
     @testset "merging" begin
-        natom = 5
         nframe = 5
+        natom = 5
         topology = TrjArray(atomid=1:natom)
         ta = TrjArray(x=rand(nframe, natom), y=rand(nframe, natom), z=rand(nframe, natom))
 
         ta_new = [topology; ta]
         ta_new["atomid 1:3"].x ≈ ta.x[:, 1:3]
     end
+end
+
+@testset "structure.jl" begin
+    @test 1 == 1
 end
 
