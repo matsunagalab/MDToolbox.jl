a7ebf4c911706322e43bafea5511c39e02e52708
ymatsunaga <ymatsunaga@riken.jp>
2020-04-21 12:24:10 +0900

    update propagate_mcmc()

--- src/dynamics.jl
+++ src/dynamics.jl
@@ -3,7 +3,7 @@ propagate_mcmc
 
 Markov Chain Monte Carlo test code
 """
-function propagate_mcmc(pot_fun::Function, proposal_fun::Function, x_current::Vector{Float64}, temperature::Float64, nstep=1)::Vector{Float64}
+function propagate_mcmc(pot_fun::Function, proposal_fun::Function, x_current::Vector{Float64}, temperature::Float64; nstep=1)::Vector{Float64}
     x = copy(x_current)
     x_proposal = similar(x)
     beta = 1 / temperature
@@ -22,6 +22,6 @@ propagate_md
 
 Molecular dynamics test code
 """
-function propagate_md(pot_fun::Function, proposal_fun::Function, x_current::Vector{Float64}, temperature::Float64, nstep=1, delta_t=1.0::Float64)
+function propagate_md(pot_fun::Function, proposal_fun::Function, x_current::Vector{Float64}; temperature::Float64, nstep=1, delta_t=1.0::Float64)
     return nothing
 end
