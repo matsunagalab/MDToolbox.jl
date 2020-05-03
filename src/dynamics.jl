82c297557239adaffa567b04530e3296e1693334
yasu <yasu@lily.local>
2020-05-03 11:05:09 +0900

    add io in dynamics.jl

--- src/dynamics.jl
+++ src/dynamics.jl
@@ -1,9 +1,21 @@
 """
+write to a trajectory file
+"""
+function flush_snapshot(io::IOStream, x::Vector{Float64}, temperature::Float64)
+    @printf(io, "%f", temperature)
+    for i in length(x)
+        @printf(io, " %f", x[i])
+    end
+    @printf(io, "\n")
+end
+
+
+"""
 propagate_mcmc
 
 Markov Chain Monte Carlo test code
 """
-function propagate_mcmc(pot_fun::Function, proposal_fun::Function, x_current::Vector{Float64}, temperature::Float64; nstep=1)::Vector{Float64}
+function propagate_mcmc(pot_fun::Function, proposal_fun::Function, x_current::Vector{Float64}, temperature::Float64; nstep=1, io=nothing, outstep=1)::Vector{Float64}
     x = copy(x_current)
     x_proposal = similar(x)
     beta = 1 / temperature
@@ -12,6 +24,9 @@ function propagate_mcmc(pot_fun::Function, proposal_fun::Function, x_current::Ve
         delta_pot = pot_fun(x_proposal) - pot_fun(x)
         if exp( -beta * delta_pot) > rand()
             x .= x_proposal
+        end
+        if isa(io, IOStream) && (mod(istep, outstep) == 0)
+            flush_snapshot(io, x, temperature)
         end
     end
     return x
