dc790299f6c629dda8c5e9a6af1924ee4276a7d7
MimoriSeica <zerons_0612@yahoo.co.jp>
2020-05-28 23:23:55 +0900

    配列に分けた

--- src/afm.jl
+++ src/afm.jl
@@ -268,37 +268,183 @@ function afmize(tra::TrjArray, config::AfmizeConfig)
     return stage
 end
 
-"""
-高速化前
+## -------------------------------------------------- afmize -----------------------------------------------------------
 
-function afmize_beta(tra::TrjArray, config::AfmizeConfig)
-    message = checkConfig(tra, config)
+struct Probe_beta{T}
+    x::AbstractArray{T}
+    y::AbstractArray{T}
+    r::AbstractArray{T}
+    angle::AbstractArray{T}
+end
+
+mutable struct Sphere_beta{T}
+    x::AbstractArray{T}
+    y::AbstractArray{T}
+    z::AbstractArray{T}
+    r::AbstractArray{T}
+end
+
+mutable struct AfmizeConfig_beta{T}
+    probeAngle::T        # Radian
+    probeRadius::T
+    range_min::Tuple{T,T}
+    range_max::Tuple{T,T}
+    resolution::Tuple{T,T}
+    atomRadiusDict::Dict{String, Float64}
+end
+
+function AfmizeConfig_beta(r::Float64)
+    return AfmizeConfig_beta(10.0 * (pi / 180),
+                            r,
+                            (-90, -90),
+                            (90, 90),
+                            (15, 15),
+                            defaultParameters())
+end
+
+function checkConfig_beta(tra::TrjArray ,config::AfmizeConfig_beta)::Union{String, Nothing}
+    if (config.range_max[1] - config.range_min[1]) % config.resolution[1] != 0
+        return "resolution.x must divide range"
+    end
+    if (config.range_max[2] - config.range_min[2]) % config.resolution[2] != 0
+        return "resolution.y must divide range"
+    end
+    if config.probeAngle == 0
+        return "probeAngle must be positive"
+    end
+
+    for name in tra.atomname
+        if !haskey(config.atomRadiusDict, name)
+            return "config dosen't know atom name $(name)"
+        end
+    end
+
+    return nothing
+end
+
+# 底を0にする
+function moveBottom_beta(atoms::Sphere_beta)
+    bottom = minimum(atoms.z .- atoms.r)
+    atoms.z .-= bottom
+end
+
+# 針を球として見たとき、どこで衝突するかの計算
+function calcCollisionAsSphere_beta(probes::Probe_beta, atoms::Sphere_beta, id::Int, h::Int, w::Int)
+    distXY = sqrt((probes.x[h, w] - atoms.x[id])^2 + (probes.y[h, w] - atoms.y[id])^2)
+    dr = probes.r[h, w] + atoms.r[id]
+    if 0 < dr^2 - distXY^2
+        return atoms.z[id] + sqrt(dr^2 - distXY^2) - probes.r[h, w]
+    else
+        return 0.0
+    end
+end
+
+# 針を円形推台として見たとき、どこで衝突するかの計算
+function calcCollisionAsCircularThrusters_beta(probes::Probe_beta, atoms::Sphere_beta, id::Int, h::Int, w::Int)
+    distXY = sqrt((probes.x[h, w] - atoms.x[id])^2 + (probes.y[h, w] - atoms.y[id])^2)
+    collisionDist = probes.r[h, w] + atoms.r[id] * cos(probes.angle[h, w])
+    if collisionDist < distXY
+        return atoms.z[id] + atoms.r[id] * sin(probes.angle[h, w]) - (distXY - collisionDist) / tan(probes.angle[h, w]) - probes.r[h, w]
+    elseif probes.r[h, w] < distXY
+        return atoms.z[id] + sqrt(atoms.r[id]^2 - (distXY - probes.r[h, w])^2) - probes.r[h, w]
+    else
+        return 0.0
+    end
+end
+
+# 衝突計算をする範囲を返す
+function calRectangle_beta(min_x, min_y, max_x, max_y, config::AfmizeConfig_beta)
+    if max_x <= config.range_min[1] || max_y <= config.range_min[2]
+        return nothing
+    end
+    if config.range_max[1] <= min_x || config.range_max[2] <= min_y
+        return nothing
+    end
+
+    min_x = max(min_x, config.range_min[1])
+    min_y = max(min_y, config.range_min[2])
+    max_x = min(max_x, config.range_max[1])
+    max_y = min(max_y, config.range_max[2])
+    
+    width  = floor(Int, (config.range_max[1] - config.range_min[1]) / config.resolution[1])
+    height = floor(Int, (config.range_max[2] - config.range_min[2]) / config.resolution[2])
+    return (max(1, floor(Int, (min_x - config.range_min[1] + config.resolution[1] - 1) / config.resolution[1])),
+            max(1, floor(Int, (min_y - config.range_min[2] + config.resolution[2] - 1) / config.resolution[2])),
+            min(width , floor(Int, (max_x - config.range_min[1] + config.resolution[1] - 1) / config.resolution[1])),
+            min(height, floor(Int, (max_y - config.range_min[2] + config.resolution[2] - 1) / config.resolution[2])))
+end
+
+
+function calRectangle_sphere_beta(atoms::Sphere_beta, config::AfmizeConfig_beta, id::Int)
+    return calRectangle_beta(atoms.x[id] - atoms.r[id] - config.probeRadius,
+                             atoms.y[id] - atoms.r[id] - config.probeRadius,
+                             atoms.x[id] + atoms.r[id] + config.probeRadius,
+                             atoms.y[id] + atoms.r[id] + config.probeRadius, config)
+end
+
+function calRectangle_circularThrusters_beta(atoms::Sphere_beta, config::AfmizeConfig_beta, id::Int)
+    dist_xy = (tan(config.probeAngle) * (atoms.z[id] + 
+               atoms.r[id] * sin(config.probeAngle) - 
+               config.probeRadius) + 
+               config.probeRadius + 
+               atoms.r[id] * cos(config.probeAngle))
+
+    return calRectangle_beta(atoms.x[id] - dist_xy, 
+                             atoms.y[id] - dist_xy,
+                             atoms.x[id] + dist_xy, 
+                             atoms.y[id] + dist_xy, config)
+end
+
+function afmize_beta(tra::TrjArray, config::AfmizeConfig_beta)
+    message = checkConfig_beta(tra, config)
     if !isnothing(message)
         println(message)
         return zeros(1, 1)
     end
 
-    width = floor(Int, (config.range_max.x - config.range_min.x) / config.resolution.x)
-    height = floor(Int, (config.range_max.y - config.range_min.y) / config.resolution.y)
-    atoms = [Sphere(tra.x[i], tra.y[i], tra.z[i],
-            config.atomRadiusDict[tra.atomname[i]]) for i in 1:tra.natom]
-    moveBottom(atoms)
+    atom_num = tra.natom
+    width = floor(Int, (config.range_max[1] - config.range_min[1]) / config.resolution[1])
+    height = floor(Int, (config.range_max[2] - config.range_min[2]) / config.resolution[2])
+    atoms = Sphere_beta([tra.x[i] for i in 1:atom_num], 
+                        [tra.y[i] for i in 1:atom_num], 
+                        [tra.z[i] for i in 1:atom_num], 
+                        [config.atomRadiusDict[tra.atomname[i]] for i in 1:atom_num])
+    println(typeof(atoms))
+    
+    moveBottom_beta(atoms)
 
     stage = zeros(height, width)
+    probes = Probe_beta([config.range_min[1] + (w-0.5) * config.resolution[1] for h in 1:height, w in 1:width],
+                        [config.range_min[2] + (h-0.5) * config.resolution[2] for h in 1:height, w in 1:width],
+                        [config.probeRadius for h in 1:height, w in 1:width],
+                        [config.probeAngle  for h in 1:height, w in 1:width])
 
-    for h in 1:height, w in 1:width
-        probe = Probe(config.range_min.x + (w-0.5) * config.resolution.x,
-                      config.range_min.y + (h-0.5) * config.resolution.y,
-                      config.probeRadius, config.probeAngle)
-        for atom in atoms
-            stage[h, w] = max(stage[h, w], calcCollisionAsSphere(probe, atom))
-            stage[h, w] = max(stage[h, w], calcCollisionAsCircularThrusters(probe, atom))
+    # 各原子事に計算する価値のある矩形を求めて、その中で探索を行う
+    
+    for atom_id in 1:atom_num
+        rectangle = calRectangle_sphere_beta(atoms, config, atom_id)
+
+        if isnothing(rectangle) continue end
+
+        for h in rectangle[2]:rectangle[4], w in rectangle[1]:rectangle[3]
+            stage[h, w] = max(stage[h, w], calcCollisionAsSphere_beta(probes, atoms, atom_id, h, w))
+        end
+    end
+
+    for atom_id in 1:atom_num
+        rectangle = calRectangle_circularThrusters_beta(atoms, config, atom_id)
+
+        if isnothing(rectangle) continue end
+
+        for h in rectangle[2]:rectangle[4], w in rectangle[1]:rectangle[3]
+            stage[h, w] = max(stage[h, w], calcCollisionAsCircularThrusters_beta(probes, atoms, atom_id, h, w))
         end
     end
 
     return stage
 end
-"""
+
+## -------------------------------------------------- afmize -----------------------------------------------------------
 
 function translateafm(afm, (dx, dy))
     afm_translated = zeros(eltype(afm), size(afm))
@@ -314,17 +460,11 @@ end
 function translateafm_periodic(afm, (dx, dy))
     afm_translated = zeros(eltype(afm), size(afm))
     (nx, ny) = size(afm)
-    for x in 1:nx
-        for y in 1:ny
-            xx = x + dx
-            yy = y + dy
-            if xx > nx 
-                xx -= nx
-            end
-            if yy > ny
-                yy -= ny
-            end
-            afm_translated[xx, yy] = afm[x, y]
+    for x in 0:nx-1
+        for y in 0:ny-1
+            xx = (x + dx + nx) % nx
+            yy = (y + dy + ny) % ny
+            afm_translated[xx+1, yy+1] = afm[x+1, y+1]
         end
     end
     afm_translated
@@ -367,6 +507,7 @@ function calcLogProb(observed, calculated, convolution_func)
 end
 
 function getafmposterior(afm::Matrix{Float64}, model_array::TrjArray, q_array::Matrix{Float64}, param_array)
+    println("in getafmposterior")
     imax_model = 0
     imax_q = 0
     best_param = param_array[1]
