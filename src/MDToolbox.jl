6722e6b7c74e4ed7f51cdf8c09a749fbc5a35ea4
ymatsunaga <ymatsunaga@riken.jp>
2021-03-14 11:11:30 +0900

    stop using Bio3DView

--- src/MDToolbox.jl
+++ src/MDToolbox.jl
@@ -36,7 +36,7 @@ export msmplot, msmgenerate, msmcountmatrix, msmtransitionmatrix, msmforward, ms
 export sp_delta_pmf, sp_design_matrix, sp_design_matrix_atom, sp_lsquares, sp_admm, sp_descent, sp_standardize!, sp_standardize, sp_cumulate_pmf, sp_cumulate_pmf_atom
 export afmize, AfmizeConfig, translateafm, getafmposterior, getposterior_parallel
 export Asd, readasd
-export get_residues, viewstruc, gpu, gpu32, cpu, cpu64, logsumexp
+export get_residues, gpu, gpu32, cpu, cpu64, logsumexp
 
 # constants
 const KB_kcalpermol = 0.0019872041 #Boltzmann constant taken from wikipedia
